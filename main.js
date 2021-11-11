3af2829327792a30800216e268d7994969646b39
Grace Gardner <graceirisgardner@gmail.com>
2021-11-11 12:56:50 -0700

    Make basic click functions for animals

--- main.js
+++ main.js
@@ -9,8 +9,44 @@ var cucumberButton = document.getElementById("cucumber");
 var playerScore = document.getElementById("playerScore");
 var computerScore = document.getElementById("computerScore");
 
+var player = new Player();
+var game = new Game();
+
 mouseButton.addEventListener("click", );
 elephantButton.addEventListener("click", );
 catButton.addEventListener("click", );
 ferretButton.addEventListener("click", );
 cucumberButton.addEventListener("click", );
+
+function mouseClick() {
+  player.selection = "mouse";
+}
+
+function elephantClick() {
+  player.selection = "elephant";
+}
+
+function catClick() {
+  player.selection = "cat";
+}
+
+function ferretClick() {
+  player.selection = "ferret";
+}
+
+function cucumberClick() {
+  player.selection = "cucumber";
+}
+
+function showHide(toShow, toHide) {
+  show(toShow);
+  hide(toHide);
+}
+
+function show(toShow) {
+  toShow.classList.remove("hidden");
+}
+
+function hide(toHide) {
+  toHide.classList.add("hidden")
+}
