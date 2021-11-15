e6c971e0e7a7aeb76157a8fb6ee9f70c514aac9f
Grace Gardner <graceirisgardner@gmail.com>
2021-11-14 19:07:23 -0700

    Add change game functionality

--- main.js
+++ main.js
@@ -18,19 +18,23 @@ var loss = document.getElementById("loss");
 var happyParty = document.getElementById("happyParty");
 var sadParty = document.getElementById("sadParty");
 var sadCloud = document.getElementById("sadCloud");
+var emojis = document.querySelectorAll(".emoji");
 
 var game = new Game();
 var player = game.player;
 var classicFighters = [mouseButton, elephantButton, catButton];
 var difficultFighters = [mouseButton, elephantButton, catButton, ferretButton, cucumberButton];
 var result = "";
+var loading = false;
 
-document.addEventListener("load", updateScore);
+document.addEventListener("load", updateScore());
+gameButton.addEventListener("click", changeGame)
+easyGame.addEventListener("click", setEasy);
+difficultGame.addEventListener("click", setDifficult);
 fighters.addEventListener("click", function(event) {
   click(event.target.alt)
 });
-easyGame.addEventListener("click", setEasy);
-difficultGame.addEventListener("click", setDifficult);
+
 
 function displayBattleZone() {
   show([document.getElementById(`${player.selection}Pick`)])
@@ -40,31 +44,37 @@ function displayBattleZone() {
 }
 
 function displayWinner() {
-  showHide([victoryZone], [battleZone])
+  showHide([victoryZone], [battleZone]);
   celebration();
 }
 
 function setEasy() {
   game.difficulty = "classic";
   game.selectDifficulty(player);
-  hide([easyGame, difficultGame]);
-  show(classicFighters)
+  hide([easyGame, difficultGame, ferretButton, cucumberButton]);
+  show([fighters]);
+  show(classicFighters);
 }
 
 function setDifficult() {
   game.difficulty = "difficult"
   game.selectDifficulty(player);
   hide([easyGame, difficultGame]);
-  show(difficultFighters)
+  show([fighters]);
+  show(difficultFighters);
 }
 
 function click(selection) {
-  player.selection = `${selection}`;
-  player.setOpponent()
-  game.declareWinner(player);
-  displayFighter(player)
-  displayBattleZone()
-  updateScore()
+  if (!loading){
+    loading = true
+    player.selection = `${selection}`;
+    player.setOpponent()
+    game.declareWinner(player);
+    displayFighter(player)
+    displayBattleZone()
+    updateScore()
+    hide([gameButton]);
+  }
 }
 
 function showHide(toShow, toHide) {
@@ -83,10 +93,6 @@ function hide(toHide) {
     toHide[i].classList.add("hidden");
   }
 }
-
-// function displayEmoji(player) {
-//   show([document.getElementById(`${player.selection}Pick`)])
-// }
 
 function displayFighter(player) {
   playerChoice.src = `./assets/${player.selection}.png`;
@@ -112,11 +118,13 @@ function updateScore() {
 
 function reset() {
   showHide([fighters], [victoryZone, sadCloud, document.getElementById(`${player.selection}Pick`)])
+  show([gameButton])
   happyParty.src = "";
   sadParty.src = "";
   player.wins = false;
+  loading = false;
 }
 
 function changeGame() {
-
+  showHide([easyGame, difficultGame], [battleZone, victoryZone, fighters])
 }
